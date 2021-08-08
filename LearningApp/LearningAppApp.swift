0907831b474bf667d45fc7a8c72c75f615c51f46
Jeehun Choi <hobbit0717@gmail.com>
2021-08-07 23:09:40 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by JH on 2021-08-07.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
