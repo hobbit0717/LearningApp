24fa8e1767b69115c472a26aa2f6f0b1ed437ab8
Jeehun Choi <hobbit0717@gmail.com>
2021-08-09 23:58:59 -0400

    - add navigation method in content model - complete content view row to re-use this view in content view - complete content view to display list view of lesson details

---
+++ LearningApp/Views/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by JH on 2021-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    
+    var body: some View {
+        
+        ScrollView {
+            LazyVStack {
+                
+                // Confirm that currentModule is set
+                if model.currentModule != nil {
+                    ForEach(0..<model.currentModule!.content.lessons.count) { index in
+                        
+                        ContentViewRow(index: index)
+                        
+                    }
+                    
+                }
+                
+                
+            }
+            .padding()
+            .navigationTitle("Learn \(model.currentModule?.category ?? "")")
+            
+            
+        }
+        
+        
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -26,9 +26,17 @@ struct HomeView: View {
                         ForEach(model.modules) { module in
                             
                             VStack (spacing: 20) {
+                                NavigationLink(
+                                    destination: ContentView()
+                                        .onAppear(perform: {
+                                            model.beginModule(module.id)
+                                        }),
+                                    label: {
+                                        // Learning Card
+                                        HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                    })
                             
-                                // Learning Card
-                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
                                 
                                 // Test Card
                                 HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
@@ -40,6 +48,7 @@ struct HomeView: View {
                         
                         
                     }
+                    .accentColor(.black)
                     .padding()
                     
                     
